0777abfeb7688a3c40c7d347b399adc2bdfb0e4c
NikonorovD <nikonorovds@yandex.ru>
2018-10-19 14:43:37 +0300

    Пакет для первой программы

---
+++ Sandbox/src/main/java/Point.java
@@ -0,0 +1,9 @@
+public class Point {
+    Point point1 = new Point();
+    point1.x=4;
+    point1.y=5;
+}
+
+class Point{
+    int x;
+}
--- Sandbox/src/main/java/ru/neolant/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/neolant/sandbox/MyFirstProgram.java
@@ -1,3 +1,5 @@
+package ru.neolant.sandbox;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
