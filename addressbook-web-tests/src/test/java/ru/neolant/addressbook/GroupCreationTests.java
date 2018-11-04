c29663f1493d710e84c9ae6c5616b60fd6add0ce
NikonorovD <nikonorovds@yandex.ru>
2018-11-04 19:59:45 +0300

    Лекция 2.3

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/GroupCreationTests.java
@@ -13,15 +13,16 @@ public class GroupCreationTests {
 
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
