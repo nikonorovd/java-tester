8a84d029088072bfd6522d0669fc965ecd5d72ad
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2018-11-23 17:56:56 +0300

    Home work №6 (ModificationContactsTests)

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/ModificationContactsTests.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/ModificationContactsTests.java
@@ -1,19 +1,10 @@
 package ru.neolant.addressbook.tests;
 
-import java.util.concurrent.TimeUnit;
-
-import org.testng.annotations.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 import ru.neolant.addressbook.model.TestBase;
 
 public class ModificationContactsTests extends TestBase {
 
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
 
     @Test
     public void testModificationContactsTests() throws Exception {
@@ -23,12 +14,6 @@ public class ModificationContactsTests extends TestBase {
         renameFirstName("Mark");
         updateContactPage();
         returnHomaPage();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wd.quit();
-
     }
 
 }
