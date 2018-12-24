c8637b3043e467558a6a822a4c3d823836e8d641
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2018-12-24 16:46:23 +0300

    Лекция 4.6. Сравнение коллекций (GroupDeletionTest не запускается)

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupDeletionTest.java
@@ -12,7 +12,7 @@ public class GroupDeletionTest extends TestBase {
     @Test
     public void testGroupDeletion() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        if (! app.getGroupHelper().isThereAgroup() )     {
+        if (!app.getGroupHelper().isThereAgroup()) {
             app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
@@ -21,6 +21,9 @@ public class GroupDeletionTest extends TestBase {
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 
 }
