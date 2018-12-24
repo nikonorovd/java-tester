67e24c5226825ff840b0c5c6b649319231fbcd9b
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2018-12-24 12:23:01 +0300

    Пройдена лекция 4.3. Количество элементов в коллекции

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.neolant.addressbook.tests.Tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.neolant.addressbook.tests.model.GroupData;
 
@@ -7,10 +8,11 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
-
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
-
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupDeletionTest.java
@@ -1,5 +1,6 @@
 package ru.neolant.addressbook.tests.Tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.neolant.addressbook.tests.model.GroupData;
 
@@ -9,12 +10,15 @@ public class GroupDeletionTest extends TestBase {
     @Test
     public void testGroupDeletion() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAgroup() )     {
             app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
         }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before - 1);
     }
 
 }
