dabc277319925ab2ff0765fe70dc918ff9b70fa9
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2018-12-25 12:55:54 +0300

    Лекция 4.6. Сравнение коллекций (GroupDeletionTest запускается)

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/appmanager/GroupHelper.java
@@ -40,7 +40,7 @@ public class GroupHelper extends HelperBase {
 
     public void selectGroup(int index) {
         wd.findElements(By.name("selected[]")).get(index).click();
-        click(By.name("selected[]"));
+//        click(By.name("selected[]"));
     }
 
     public void initGroupModification() {
