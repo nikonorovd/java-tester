8a92c477368834abc0497c915e6e9b0618f959a0
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2019-01-24 14:20:49 +0300

    Lesson 4.7(+)

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/model/GroupData.java
@@ -1,14 +1,52 @@
 package ru.neolant.addressbook.tests.model;
 
 public class GroupData {
+    private final String id;
     private final String groupname;
     private final String header;
     private final String footer;
 
     public GroupData(String groupname, String header, String footer) {
+        this.id = null;
         this.groupname = groupname;
         this.header = header;
         this.footer = footer;
+    }
+    public GroupData(String id, String groupname, String header, String footer) {
+        this.id = id;
+        this.groupname = groupname;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", groupname='" + groupname + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+        return groupname != null ? groupname.equals(groupData.groupname) : groupData.groupname == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (groupname != null ? groupname.hashCode() : 0);
+        return result;
     }
 
     public String getGroupname() {
@@ -23,25 +61,4 @@ public class GroupData {
         return footer;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        return groupname != null ? groupname.equals(groupData.groupname) : groupData.groupname == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return groupname != null ? groupname.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "groupname='" + groupname + '\'' +
-                '}';
-    }
 }
