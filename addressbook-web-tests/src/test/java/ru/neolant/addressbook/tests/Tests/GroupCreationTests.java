4759bd04122e347afb853addef9ad1600461ae18
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2019-01-24 09:18:31 +0300

    Lesson 4.3 (-)

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.neolant.addressbook.tests.Tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.neolant.addressbook.tests.model.GroupData;
 
@@ -7,10 +8,11 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
-
+        int before = app.getGroupHelper().getGroupCount();
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
-
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
