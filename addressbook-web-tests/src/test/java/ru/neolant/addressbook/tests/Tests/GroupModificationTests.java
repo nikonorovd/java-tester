7c94cf4ffbad7625b4b5feae2411753be816907a
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2019-01-24 09:31:31 +0300

    Lesson 4/3(++)

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.neolant.addressbook.tests.Tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.neolant.addressbook.tests.model.GroupData;
 
@@ -7,6 +8,7 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAgroup() )     {
             app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
         }
@@ -15,6 +17,8 @@ public class GroupModificationTests extends TestBase {
         app.getGroupHelper().fillGroupForm(new GroupData("Test1", "Test1", "Test1"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before );
 
     }
 }
