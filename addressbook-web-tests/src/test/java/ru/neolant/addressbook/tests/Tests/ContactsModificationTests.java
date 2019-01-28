3043f0558d513885234673301c46dcd06c326d5d
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2019-01-28 13:07:58 +0300

    Реализована проверка на контроль количества элементов до и после модификации контактов.

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/ContactsModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/ContactsModificationTests.java
@@ -11,12 +11,12 @@ public class ContactsModificationTests extends TestBase{
 
     @Test
     public void testContactsModification() {
-        int before = app.getContactHelper().getContactCount();
-//        app.getNavigationHelper().gotoAddNew();
+        app.getNavigationHelper().gotoAddNew();
         if (!app.getContactHelper().isThereAContact()){
             app.getContactHelper().createContact(new ContactData("first name", "middle name", "last name",
                     "company", "Titova", "1212121", "555666", "info@info.ru", "test1"),true);
         }
+        int before = app.getContactHelper().getContactCount();
         app.getContactHelper().editContact();
         app.getContactHelper().fillContactsForm(new ContactData("first name", "middle name", "last name", "company", "Titova", "1212121", "555666", "info@info.ru", null),false);
         app.getContactHelper().updateContacts();
