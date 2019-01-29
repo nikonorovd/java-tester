6659f0a7741cc2844140c06b320968464295330b
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2019-01-29 14:19:06 +0300

    Удаление контактов работает. 4.5 пройдена

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/ContactsDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/ContactsDeletionTests.java
@@ -11,6 +11,7 @@ public class ContactsDeletionTests extends TestBase {
 
     @Test
     public void testContactsDeletion() throws Exception {
+        app.getNavigationHelper().gotoAddNew();
         if (!app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("first name", "middle name", "last name",
                     "company", "Titova", "1212121", "555666", "info@info.ru", "test1"), true);
@@ -18,6 +19,7 @@ public class ContactsDeletionTests extends TestBase {
         List<ContactData> before = app.getContactHelper().getContactList();
             app.getContactHelper().selectContact(before.size() -1);
         app.getContactHelper().deletionContact();
+        app.getContactHelper().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() - 1);
     }
