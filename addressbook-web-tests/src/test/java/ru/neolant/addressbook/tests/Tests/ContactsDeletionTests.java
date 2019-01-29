812632c5174e53d758c16a36bca148d4cbba8a4e
nikonorovd <44136665+nikonorovd@users.noreply.github.com>
2019-01-29 15:29:49 +0300

    Сравнение списков до удаления и после удаления целиком. 4.6 (удаление +)

--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/ContactsDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/Tests/ContactsDeletionTests.java
@@ -22,6 +22,10 @@ public class ContactsDeletionTests extends TestBase {
         app.getContactHelper().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() -1);
+            Assert.assertEquals(before, after);
+        }
     }
 
-}
+
--- addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/neolant/addressbook/tests/model/ContactData.java
@@ -11,6 +11,7 @@ public class ContactData {
     private final String email;
     private String group;
 
+
     public ContactData(String firstname, String middlename, String lastname, String company, String address, String home, String modile, String email, String group) {
         this.firstname = firstname;
         this.middlename = middlename;
@@ -57,5 +58,36 @@ public class ContactData {
 
     public String getGroup() {
         return group;
+    }
+
+
+
+    @Override
+    public String toString(){
+        return"ContactData{"+
+        "firstname='"+firstname+'\''+
+        '}';
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        if (middlename != null ? !middlename.equals(that.middlename) : that.middlename != null) return false;
+        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+        return company != null ? company.equals(that.company) : that.company == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstname != null ? firstname.hashCode() : 0;
+        result = 31 * result + (middlename != null ? middlename.hashCode() : 0);
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + (company != null ? company.hashCode() : 0);
+        return result;
     }
 }
